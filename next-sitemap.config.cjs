a405dd3f73b7036b40a340c7c15c70faaec3d63e
Asuka Watanabe <asuka@Asukas-MacBook-Pro.local>
2025-07-24 19:56:52 +0200

    adjusting the site url sections

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,7 +1,9 @@
-const SITE_URL =
-  process.env.NEXT_PUBLIC_SERVER_URL ||
-  process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-  'https://example.com'
+// const SITE_URL =
+//   process.env.NEXT_PUBLIC_SERVER_URL ||
+//   process.env.VERCEL_PROJECT_PRODUCTION_URL ||
+//   'https://asukamethod.com'
+
+const SITE_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'https://asukamethod.com'
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {

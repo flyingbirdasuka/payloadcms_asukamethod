83bb7128ac8c6e214c588c7c0e2ff88cd5194b84
渡邉明日香 <watanabeasuka@mac-1.home>
2025-07-30 10:51:05 +0200

    trigger the webhook only from booking form

--- src/collections/Bookings/hooks/submitBooking.ts
+++ src/collections/Bookings/hooks/submitBooking.ts
@@ -1,36 +1,41 @@
 import type { CollectionAfterChangeHook } from 'payload'
 
 
-export const submitBooking: CollectionAfterChangeHook = async ({ doc, req }) => {
+export const submitBooking: CollectionAfterChangeHook = async ({ doc, req, operation, previousDoc }) => {
   console.log('submitBooking hook triggered');
-    try {
-      if (!doc.selectedDates?.length) return;
+    // Only run on create
+  if (operation !== 'create') {
+    console.log('Skipping webhook: Not a creation');
+    return;
+  }
+  try {
+    if (!doc.selectedDates?.length) return;
 
-      const fullClasses = await Promise.all(
-        doc.selectedDates
-          .map((entry: any) => (typeof entry === 'object' && entry.id ? entry.id : entry))
-          .map((id: string | number) =>
-            req.payload.findByID({ collection: 'online-classes', id })
-          )
-      )
-      const filteredClasses = fullClasses.filter(Boolean);
+    const fullClasses = await Promise.all(
+      doc.selectedDates
+        .map((entry: any) => (typeof entry === 'object' && entry.id ? entry.id : entry))
+        .map((id: string | number) =>
+          req.payload.findByID({ collection: 'online-classes', id })
+        )
+    )
+    const filteredClasses = fullClasses.filter(Boolean);
 
-      const webhookPayload = {
-        ...doc,
-        selectedDates: filteredClasses,
-      };
-      const res = await fetch(`${process.env.WEBHOOK_AUTOMATION_PAYMENT}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(webhookPayload),
-      });
+    const webhookPayload = {
+      ...doc,
+      selectedDates: filteredClasses,
+    };
+    const res = await fetch(`${process.env.WEBHOOK_AUTOMATION_PAYMENT}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(webhookPayload),
+    });
 
-      if (!res.ok) {
-        console.error(`n8n webhook error: ${res.status} ${res.statusText}`);
-      } else {
-        console.log('Webhook sent successfully');
-      }
-    } catch (err) {
-      console.error('Error in afterChange hook:', err);
+    if (!res.ok) {
+      console.error(`n8n webhook error: ${res.status} ${res.statusText}`);
+    } else {
+      console.log('Webhook sent successfully');
     }
+  } catch (err) {
+    console.error('Error in afterChange hook:', err);
+  }
 }
